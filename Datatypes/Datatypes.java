dd81867d9ae46200e13ffded77c2e7d0e770ffa3
Pranali Thorat <100304254+Pranali34@users.noreply.github.com>
2024-01-02 10:22:13 +0530

    Add files via upload

---
+++ Datatypes/Datatypes.java
@@ -0,0 +1,40 @@
+public class Datatypes
+{
+	public static void main(String args[])
+    	{
+		byte bNum = -12;
+		short sNum = 3434;
+		int iNum = 430723475;
+		long lNum = 5647389;
+		float fNum = 206.3434f;
+		double dNum = 532.683462767356763;
+		char Ch = 'P';
+		Boolean Val = true;
+	
+		System.out.println("\n****************************************\n");
+
+		System.out.println(bNum);
+		System.out.println(sNum);
+		System.out.println(iNum);
+		System.out.println(lNum);
+		System.out.println(fNum);
+		System.out.println(dNum);
+		System.out.println(Ch);
+		System.out.println(Val);
+
+		System.out.println("\n****************************************\n");
+
+		System.out.println("bNum is a byte Type of Variable = " +bNum);
+		System.out.println("sNum is a short Type of Variable = " +sNum);
+		System.out.println("iNum is a int Type of Variable = " +iNum);
+		System.out.println("lNum is a long Type of Variable = " +lNum);
+		System.out.println("fNum is a float Type of Variable = " +fNum);
+		System.out.println("dNum is a double Type of Variable = " +dNum);
+		System.out.println("Ch is a char Type of Variable = " +Ch);
+		System.out.println("Val is a Boolean Type of Variable = " +Val);
+
+		System.out.println("\n****************************************\n");
+
+	}
+	    
+}
